ff2b24da10bd699213abd6e1ce2f8cd963dfd5c1
tssarath <tssarath@amazon.com>
2018-08-23 16:14:59 +0530

    master1

--- fifth.java
+++ fifth.java
@@ -1,33 +1,57 @@
-A.ajax("${siteconfig.submitReconciliationFormData}", {
-		method : 'get',
-		params : {
-		stationId : A.$("#modalStationId_" + cashSubmitDate).val(),
-		depositAmountUnit : A.$("#modalDepositAmountUnit_" + cashSubmitDate).val(),
-		depositAmountValue : A.$("#modalDepositAmountValue_" + cashSubmitDate).val(),
-		depositSlipNumber : A.$("#modalDepositNumber_" + cashSubmitDate).val(),
-		shipmentCount : A.$("#modalShipmentCount_" + cashSubmitDate).val(),
-		depositerLogin: A.$("#modalDepositerLogin_" + cashSubmitDate).val(),
-		mposDepositAmountUnit : A.$("#modalMPOSDepositAmountUnit_" + cashSubmitDate).val(),
-		mposDepositAmountValue : A.$("#modalMPOSDepositAmountValue_" + cashSubmitDate).val(),
-		mposShipmentCount : A.$("#modalMPOSShipmentCount_" + cashSubmitDate).val(),
-		cashSubmitDate : A.$("#modalCashSubmitDate_" + cashSubmitDate).val()
-		},
-		success : function(data) {
-		A.$(".loader").hide();
-		alert("Data saved!");
-<<<<<<< HEAD
-		A.$("#submitButton").click();
-=======
-		//A.$("#submitButton").click();
-		A.$(lolololol);
->>>>>>> 3
+<%@page import="java.util.HashMap"%>
+<%@page contentType="text/html" pageEncoding="UTF-8" session="false"%>
+<%@taglib prefix="m"
+		uri="http://schema.amazon.com/xmlns/matrix/nested-components-1.0"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="a" uri="aui"%>
+<jsp:useBean id="cons" class="com.amazon.simplestack.cashmanageruiapp.webapp.constants.Constants" scope="page"/>
 
-		A.$("[data-action='a-popover-close']").click();
-		},
-		error : function() {
-		A.$(".loader").hide();
-		event.$target.context.disabled = false;
-		alert('There was error in getting the data');
-		}
-		});
-});
+<a:gridRow>
+<a:gridColumn gridUnits="3" textAlign="${'right'}">
+		Station
+</a:gridColumn>
+<a:gridColumn gridUnits="3">
+<a:dropdown nativeId="stationSelect" name="stationSelect"
+		optionPrompt="Select a station">
+<c:forEach items="${stationsList}" var="station">
+<c:if test="${station.getName() ne cons.UNASSIGNED_STATION}">
+<a:dropdownOption value="${station.getId()}">${station.getName()}</a:dropdownOption>
+</c:if>
+</c:forEach>
+</a:dropdown>
+</a:gridColumn>
+<a:gridColumn gridUnits="3" textAlign="${'right'}">
+		Date
+</a:gridColumn>
+<a:gridColumn gridUnits="3" position="${'last'}">
+<a:calendar id="calendarInput" />
+</a:gridColumn>
+</a:gridRow>
+<a:gridRow spacingTop="${'base'}">
+<a:gridColumn gridUnits="6" textAlign="${'center'}">
+<a:button value="Submit" htmlButtonType="${'button'}" id="submitButton">Submit to Bank</a:button>
+</a:gridColumn>
+</a:gridRow>
+
+<a:gridRow spacingTop="${'base'}">
+<div class="dataTables_wrapper" id="tableListContainer"></div>
+</a:gridRow>
+
+<script>
+	P.when('A').execute('aui-ready', function(A) {
+			A.$("#submitButton").click(function() {
+			P.when('a-dropdown').execute(function(dropdown) {
+
+			var stationID = dropdown.getSelect("stationSelect").val();
+			if(isEmpty(stationID)){
+			alert("Please select a station");
+			return;
+			}
+			else{
+			alert("Selected: " + stationID);
+			}
+
+			});
+			});
+			});
+</script>
