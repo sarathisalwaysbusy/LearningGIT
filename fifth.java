6ec5dcf6338f08b00faffaf5a7a834d23b67c050
tssarath <tssarath@amazon.com>
2018-08-23 16:17:08 +0530

    feature testing

--- fifth.java
+++ fifth.java
@@ -1,29 +1,84 @@
-A.ajax("${siteconfig.submitReconciliationFormData}", {
-		method : 'get',
-		params : {
-		stationId : A.$("#modalStationId_" + cashSubmitDate).val(),
-		depositAmountUnit : A.$("#modalDepositAmountUnit_" + cashSubmitDate).val(),
-		depositAmountValue : A.$("#modalDepositAmountValue_" + cashSubmitDate).val(),
-		depositSlipNumber : A.$("#modalDepositNumber_" + cashSubmitDate).val(),
-		shipmentCount : A.$("#modalShipmentCount_" + cashSubmitDate).val(),
-		depositerLogin: A.$("#modalDepositerLogin_" + cashSubmitDate).val(),
-		mposDepositAmountUnit : A.$("#modalMPOSDepositAmountUnit_" + cashSubmitDate).val(),
-		mposDepositAmountValue : A.$("#modalMPOSDepositAmountValue_" + cashSubmitDate).val(),
-		mposShipmentCount : A.$("#modalMPOSShipmentCount_" + cashSubmitDate).val(),
-		cashSubmitDate : A.$("#modalCashSubmitDate_" + cashSubmitDate).val()
-		},
-		success : function(data) {
-		A.$(".loader").hide();
-		alert("Data saved!");
-		//A.$("#submitButton").click();
-		A.$(lolololol);
+<%@page import="java.util.HashMap"%>
+<%@page contentType="text/html" pageEncoding="UTF-8" session="false"%>
+<%@taglib prefix="m"
+		uri="http://schema.amazon.com/xmlns/matrix/nested-components-1.0"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="a" uri="aui"%>
+<jsp:useBean id="cons" class="com.amazon.simplestack.cashmanageruiapp.webapp.constants.Constants" scope="page"/>
 
-		A.$("[data-action='a-popover-close']").click();
-		},
-		error : function() {
-		A.$(".loader").hide();
-		event.$target.context.disabled = false;
-		alert('There was error in getting the data');
-		}
-		});
-});
+<a:gridRow>
+<a:gridColumn gridUnits="1" textAlign="${'right'}">
+		Station
+</a:gridColumn>
+<a:gridColumn gridUnits="3">
+<a:dropdown nativeId="stationSelect" name="stationSelect"
+		optionPrompt="Select a station">
+<c:forEach items="${stationsList}" var="station">
+<c:if test="${station.getName() ne cons.UNASSIGNED_STATION}">
+<a:dropdownOption value="${station.getId()}">${station.getName()}</a:dropdownOption>
+</c:if>
+</c:forEach>
+</a:dropdown>
+</a:gridColumn>
+
+<a:gridColumn gridUnits="1" textAlign="${'right'}">
+		Select Date
+</a:gridColumn>
+
+<a:gridColumn gridUnits="3">
+<a:calendar id="calendarStartInput" />
+</a:gridColumn>
+
+<a:gridColumn gridUnits="3">
+<a:calendar id="calendarEndInput" />
+</a:gridColumn>
+
+</a:gridRow>
+<a:gridRow spacingTop="${'base'}">
+<a:gridColumn gridUnits="3" textAlign="${'right'}">
+<a:button value="Submit" htmlButtonType="${'button'}" id="getDetailsButton">Get Deposit Details</a:button>
+</a:gridColumn>
+</a:gridRow>
+
+<a:gridRow spacingTop="${'base'}">
+<div class="dataTables_wrapper" id="tableListContainer"></div>
+</a:gridRow>
+
+<script>
+	P.when('A').execute('aui-ready', function(A) {
+			A.$("#getDetailsButton").click(function() {
+			P.when('a-dropdown').execute(function(dropdown) {
+
+			var stationID = dropdown.getSelect("stationSelect").val();
+			if(isEmpty(stationID)){
+			alert("Please select a station");
+			return;
+			}
+			var calendarStartInput = getCalendarInputString(A, "calendarStartInput");
+			var calendarEndInput = getCalendarInputString(A, "calendarEndInput");
+			if(calendarStartInput == null || calendarEndInput == null){
+			alert("Please enter a valid start and end dates");
+			return;
+			}
+			//TODO: check for <=present date
+
+			var dateDifference = getDateDifferenceBetweenTwoCalendarIds(A, "calendarStartInput", "calendarEndInput");
+			showLoader(A);
+			A.ajax("${siteconfig.reconciliationUpdateTable}", {
+			method : 'get',
+			params : {
+			stationId : stationID
+			},
+			success : function(data) {
+			hideLoader(A);
+			A.$("#tableListContainer").html(data);
+			},
+			error : function() {
+			hideLoader(A);
+			alert('There was error in getting the data');
+			}
+			});
+			});
+			});
+			});
+</script>
